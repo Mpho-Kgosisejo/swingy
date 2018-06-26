4c76ea9378b35395ba26ddc8a57265c7c6b514ae
Mpho KGOSISEJO <mkgosise@e4r9p11.wethinkcode.co.za>
2018-06-26 18:33:23 +0200

    more gui updates -_-

--- src/main/java/packages/storage/Hero.java
+++ src/main/java/packages/storage/Hero.java
@@ -1,13 +1,13 @@
 package packages.storage;
 
-import packages.utils;
+// import packages.utils;
 import java.io.*;
 
 
 public class Hero
 {
-    Hero(String name, HeroType type, int level, int xPoints, String weapon, String armor)
-    {
+    // Hero(String name, HeroType type, int level, int xPoints, String weapon, String armor)
+    // {
         
-    }
+    // }
 }
--- src/main/java/packages/utils/Type.java
+++ src/main/java/packages/utils/Type.java
@@ -2,14 +2,14 @@ package packages.utils;
 
 public class Type
 {
-    public enum HeroType
-    {
-        HERO_TYPE_1("knight"),
-        HERO_TYPE_2("warrior"),
-        HERO_TYPE_3("elf"),
-        HERO_TYPE_4("hunter"),
-        HERO_TYPE_5("Villager");
+    // public enum HeroType
+    // {
+    //     HERO_TYPE_1("knight"),
+    //     HERO_TYPE_2("warrior"),
+    //     HERO_TYPE_3("elf"),
+    //     HERO_TYPE_4("hunter"),
+    //     HERO_TYPE_5("Villager");
         
-    }
+    // }
 }
         
